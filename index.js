1b3b4c15e3d2a6149a4d88e7176262eab8c45153
Harikrishnan MP <harik013@gmail.com>
2024-12-17 05:26:39 +0000

    hk 1

--- index.js
+++ index.js
@@ -19,6 +19,28 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.post("/api/shorturl", (req, res) => {
+  const originalUrl = req.body.original_url;
+
+  if (!originalUrl || !isValidUrl(originalUrl)) {
+      return res.status(400).json({ error: "Invalid URL" });
+  }
+
+  const shortUrlId = currentId++;
+  
+  urlDatabase[shortUrlId] = originalUrl;
+
+  res.json({
+      original_url: originalUrl,
+      short_url: shortUrlId
+  });
+});
+
+function isValidUrl(url) {
+  const regex = /^(ftp|http|https):\/\/[^ "]+$/;
+  return regex.test(url);
+}
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
